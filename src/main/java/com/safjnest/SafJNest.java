51e4db1a8145261f57f9a7b5f35e6d69d49cb1d1
LΞON <leonardopanichi4@gmail.com>
2022-02-03 18:29:01 +0100

    .

--- src/main/java/com/safjnest/SafJNest.java
+++ src/main/java/com/safjnest/SafJNest.java
@@ -51,6 +51,22 @@ public class SafJNest extends Thread {
         }
     }
 
+    public static boolean isInteger(String s) {
+        return isInteger(s,10);
+    }
+    
+    public static boolean isInteger(String s, int radix) {
+        if(s.isEmpty()) return false;
+        for(int i = 0; i < s.length(); i++) {
+            if(i == 0 && s.charAt(i) == '-') {
+                if(s.length() == 1) return false;
+                else continue;
+            }
+            if(Character.digit(s.charAt(i),radix) < 0) return false;
+        }
+        return true;
+    }
+
     public static boolean intIsParsable(String input) {
         try {
             Integer.parseInt(input);
