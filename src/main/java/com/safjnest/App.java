0d3634fd3bfe549f91939d1ef4ad0f2bb9457922
LΞON <leonardopanichi4@gmail.com>
2022-02-05 12:22:00 +0100

    remove error

--- src/main/java/com/safjnest/App.java
+++ src/main/java/com/safjnest/App.java
@@ -16,7 +16,6 @@ import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.managers.AudioManager;
@@ -123,7 +122,6 @@ public class App extends ListenerAdapter{
                 } catch (Exception e) {
                     channel.sendMessage(e.getMessage()).queue();
                 }
-                channel.sendMessage("Errore").queue();
             break;
 
             case "permissions":
