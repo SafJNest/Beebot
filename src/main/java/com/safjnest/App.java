0e0793d1faf8b92b1bdf15cf98e74a5a3de3043f
Lorenzo Sanseverino <lorenzosanseverino2003@gmail.com>
2022-02-23 18:17:33 +0100

    a

--- src/main/java/com/safjnest/App.java
+++ src/main/java/com/safjnest/App.java
@@ -42,16 +42,16 @@ import net.dv8tion.jda.api.utils.MemberCachePolicy;
 public class App extends ListenerAdapter {
     private static JDA jda;
     private static String token;
-    private static String PREFIX = "$";
-    //private static Activity activity = Activity.playing("Outplaying other bots | " + PREFIX + "help");
-    private static Activity activity = Activity.playing("waiting for jellyWX");
+    private static String PREFIX = "%";
+    private static Activity activity = Activity.playing("Outplaying other bots | " + PREFIX + "help");
+    //private static Activity activity = Activity.playing("waiting for jellyWX");
     private static final int maxBighi = 11700;
     private static final int maxPrime = (int) Integer.valueOf(maxBighi/5).floatValue();
     private static HashMap<String, String> tierOneLink = new HashMap<>();
     public static void main(String[] args) throws LoginException {
-        String tokenCanary = "OTM5ODc2ODE4NDY1NDg4OTI2.Yf_Ofw.1Ql5INVXqLSPXYG7OxRaCD5A8bU";
-        token  = tokenCanary;
-        //token = args[0];
+        //String tokenCanary = "OTM5ODc2ODE4NDY1NDg4OTI2.Yf_Ofw.1Ql5INVXqLSPXYG7OxRaCD5A8bU";
+        ///token  = tokenCanary;
+        token = args[0];
         jda = JDABuilder
                 .createLight(token, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES, GatewayIntent.GUILD_VOICE_STATES, GatewayIntent.GUILD_MEMBERS)
                 .addEventListeners(new TheListener())
