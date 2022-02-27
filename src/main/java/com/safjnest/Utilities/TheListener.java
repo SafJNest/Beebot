08d4787cb999c1c58d009a0e90b31a4ed25c8f29
Lorenzo Sanseverino <lorenzosanseverino2003@gmail.com>
2022-02-27 19:43:14 +0100

    add new event when  a user joind the server

--- src/main/java/com/safjnest/Utilities/TheListener.java
+++ src/main/java/com/safjnest/Utilities/TheListener.java
@@ -1,5 +1,11 @@
 package com.safjnest.Utilities;
 
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberUpdateEvent;
+import net.dv8tion.jda.api.events.guild.member.update.GenericGuildMemberUpdateEvent;
+import net.dv8tion.jda.api.events.guild.member.update.GuildMemberUpdateBoostTimeEvent;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -15,5 +21,22 @@ public class TheListener extends ListenerAdapter{
         if(e.getGuild().getAudioManager().isConnected() && e.getChannelLeft().getMembers().size() == 1 ){
             e.getGuild().getAudioManager().closeAudioConnection();
         }
+    }
+
+    @Override // USE THIS WHEN YOU WANT TO OVERRIDE A METHOD
+    public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+        User newguy = event.getUser();
+        TextChannel welcome = event.getGuild().getSystemChannel();
+        welcome.sendMessage(
+            "Ehi " + newguy.getAsMention() + ", Benvenuto nel "+event.getGuild().getName() +".\n"
+            + "Per iniziare ottieni il verificato nella stanza " + event.getGuild().getTextChannelById("706174812690579497").getAsMention()
+            + ", una volta fatto avrai accesso alle varie stanze del server.\nRicorda di seguire le regole e per qualsiasi informazione contatta i vari admin.").queue();
+    }
+
+    
+    public void onGuildMemberUpdateBoostTime​(GuildMemberUpdateBoostTimeEvent event){
+        User newguy = event.getUser();
+        TextChannel welcome = event.getGuild().getSystemChannel();
+        welcome.sendMessage("NO FUCKING WAY " + newguy.getAsMention() + " HA BOOSTATO IL SERVER!!\n" + event.getGuild().getBoostCount()).queue();
     }
 }
