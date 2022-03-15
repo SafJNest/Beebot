2848b1351da942b790f2e7324fbaf048a64fa59b
NeutronSun <lorenzosanseverino2003@gmail.com>
2022-03-15 11:40:23 +0100

    cose

--- src/main/java/com/safjnest/Commands/Audio/Upload.java
+++ src/main/java/com/safjnest/Commands/Audio/Upload.java
@@ -1,5 +1,20 @@
 package com.safjnest.Commands.Audio;
 
+import java.io.File;
+
+//import javax.xml.crypto.dsig.keyinfo.KeyName;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import com.safjnest.Utilities.FileListener;
@@ -11,6 +26,9 @@ import com.safjnest.Utilities.FileListener;
  * @since 1.2.5
  */
 public class Upload extends Command{
+
+    private static String bucketName = "thebeebox";
+    private static String keyName = "tre.mp3";
 
     public Upload(){
         this.name = "upload";
@@ -26,5 +44,32 @@ public class Upload extends Command{
         event.reply("operativo e pronto a listenare");
         FileListener listino = new FileListener(event.getArgs());
         event.getJDA().addEventListener(listino);
+        
+        AWSCredentials credentials = new BasicAWSCredentials("AKIASJG3D4LSZMKR7L4R", "zufmhZG5m8QhDZCeBYALs2S1wOu/x9zgoYxjbZIV");
+        ClientConfiguration clientConfiguration = new ClientConfiguration();
+        clientConfiguration.setSignerOverride("AWSS3V4SignerType");
+
+        AmazonS3 s3Client = AmazonS3ClientBuilder
+            .standard()
+            .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("s3.us-east-1.amazonaws.com", "us-east-1"))
+            .withPathStyleAccessEnabled(true)
+            .withClientConfiguration(clientConfiguration)
+            .withCredentials(new AWSStaticCredentialsProvider(credentials))
+            .build();
+            
+        try {
+            System.out.println("Uploading a new object to S3 from a file\n");
+            File file = new File("Upload\\eee.mp3");
+            // Upload file
+            PutObjectRequest request = new PutObjectRequest(bucketName, keyName, file);
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType("audio/mpeg");
+            metadata.addUserMetadata("name", "eee");
+            metadata.addUserMetadata("category", "dio");
+            request.setMetadata(metadata);
+            s3Client.putObject(request);
+        }catch(AmazonClientException ace){
+            ace.printStackTrace();
+        }
 	}
 }
