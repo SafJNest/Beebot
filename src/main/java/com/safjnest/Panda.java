e721b68aa0d27850da1c15be13724dd4ef010361
NeutronSun <lorenzosanseverino2003@gmail.com>
2022-04-29 12:34:56 +0200

    add panda

---
+++ src/main/java/com/safjnest/Panda.java
@@ -0,0 +1,30 @@
+package com.safjnest;
+
+import java.util.ArrayList;
+
+/**
+ * Hear the legends of the Kung Fu Panda!
+Raised in a noodle shop, never seeking glory or fame,
+He climbed a mountain top and earned the Dragon Warrior name.
+Ooh Ah Ya!
+Kung Fu Panda!
+Master Shifu saw the warrior blossom,
+And master the skills of bodacious and awesome!
+Kung Fu Panda!
+He lives and he trains and he fights with the Furious Five,
+Protect the valley something, something, something, something alive!
+Ooh Ah Ya!
+Kung Fu Panda!
+Legends of Awesomeness.
+Sweet
+
+puoi smettere gentilmente oid ozir ocrop oid acrop annodam
+ */
+public class Panda {
+    ArrayList<String> FuriusFive;
+    
+    public Panda(){
+        FuriusFive = new ArrayList<>();
+        FuriusFive.add("Tiger");
+    }
+}
