fb8200a822bc465e234d11e46058628e4a75aeeb
NeutronSun <lorenzosanseverino2003@gmail.com>
2022-05-12 22:19:00 +0200

    add move command, improve calc json, improve help, add info command wriyhgv

---
+++ src/main/java/com/safjnest/Commands/ManageMembers/Move.java
@@ -0,0 +1,75 @@
+package com.safjnest.Commands.ManageMembers;
+
+import java.util.List;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.safjnest.Utilities.JSONReader;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.VoiceChannel;
+
+/**
+ * @author <a href="https://github.com/NeutronSun">NeutroSun</a>
+ * 
+ * @since 1.3
+ */
+public class Move extends Command{
+
+    public Move(){
+        this.name = this.getClass().getSimpleName();
+        this.aliases = new JSONReader().getArray(this.name, "alias");
+        this.help = new JSONReader().getString(this.name, "help");
+        this.cooldown = new JSONReader().getCooldown(this.name);
+        this.category = new Category(new JSONReader().getString(this.name, "category"));
+        this.arguments = new JSONReader().getString(this.name, "arguments");
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        User theGuy = null;
+        List<Member> theGuys = null;
+        VoiceChannel channel = null;
+        String[] args = event.getArgs().split(" ");
+        if(args[0].equalsIgnoreCase("me"))
+            theGuy = event.getAuthor();
+        else if(args[0].equalsIgnoreCase("here")){
+            theGuys = event.getMember().getVoiceState().getChannel().getMembers();
+            for(Member member : theGuys){
+                System.out.println(member.getUser().getName());
+            }
+        }else if(event.getMessage().getMentionedUsers().size() > 0)
+            theGuy = event.getMessage().getMentionedUsers().get(0);
+        else{
+            event.reply("Non ho capito a chi vuoi spostare.");
+            return;
+        }
+        //get the channel
+        if(args[1].equalsIgnoreCase("")){
+            event.reply("Non ho capito a dove vuoi spostare.");
+            return;
+        }else if(args[1].equalsIgnoreCase("here")){
+            channel = event.getGuild().getVoiceChannelById(event.getMember().getVoiceState().getChannel().getId());
+        }else{
+            channel = event.getGuild().getVoiceChannelById(args[1]);
+            if(channel == null){
+                event.reply("Non ho trovato il canale.");
+                return;
+            }else if(channel.getId().equals(event.getMember().getVoiceState().getChannel().getId())){
+                event.reply("Sei già in quel canale odizor");
+                return;
+            }
+        }
+        if(theGuy == null){
+            for(Member member : theGuys){
+                event.getGuild().moveVoiceMember(member, channel).queue();
+                event.reply(member.getAsMention() + " spostato in: " + channel.getName());
+            }
+            return;
+        }
+        event.getGuild().moveVoiceMember(event.getGuild().getMember(theGuy), channel).queue();
+        event.reply(theGuy.getAsMention() + " spostato in: " + channel.getName());
+
+    }
+}
