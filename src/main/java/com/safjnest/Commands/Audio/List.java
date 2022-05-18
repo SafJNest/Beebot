6d923d76de8f6d23c0cb1e35fcb8ef942f3d8cfe
Lorenzo Sanseverino <lorenzosanseverino2003@gmail.com>
2022-05-18 16:04:21 +0200

    improve list

--- src/main/java/com/safjnest/Commands/Audio/List.java
+++ src/main/java/com/safjnest/Commands/Audio/List.java
@@ -1,19 +1,14 @@
 package com.safjnest.Commands.Audio;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
-import java.awt.Color;
 
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import com.safjnest.Utilities.AwsS3;
 import com.safjnest.Utilities.JSONReader;
-
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.MessageChannel;
 
 /**
  * @author <a href="https://github.com/NeutronSun">NeutronSun</a>
@@ -37,36 +32,30 @@ public class List extends Command {
 	protected void execute(CommandEvent event) {
         HashMap<String, ArrayList<String>> alpha = null;
         String[] args = event.getArgs().split(" ");
-        if(args[0].equals("me"))
-            alpha = s3Client.listObjectsByServer(event.getAuthor().getId(), event);
-        else
-            alpha = s3Client.listObjects(event.getGuild().getId());
-        
-        Map<String, ArrayList<String>> sortedMap = new TreeMap<>(alpha);
-        sortedMap.putAll(alpha);
-        MessageChannel channel = event.getChannel();
-        EmbedBuilder eb = new EmbedBuilder();
-        eb.setTitle("SoundBoard");
+        boolean isListMe = args[0].equals("me");
         String soundNames = "";
+        if(isListMe){
+            alpha = s3Client.listObjectsByServer(event.getAuthor().getId(), event);
+            Map<String, ArrayList<String>> sortedMap = new TreeMap<>(alpha);
+            sortedMap.putAll(alpha);
             for(String k : sortedMap.keySet()) {
                 soundNames += "**"+ k +"**" + ":\n";
                 for(String s : sortedMap.get(k)){
                     soundNames+= s + " - ";
                 }
-                soundNames+="\n";
-                //eb.addField(k, soundNames, true);
+                soundNames += "\n";
             }
-            System.out.println(soundNames);
+        }else{
+            alpha = s3Client.listObjects(event.getGuild().getId());
+            Map<String, ArrayList<String>> sortedMap = new TreeMap<>(alpha);
+            sortedMap.putAll(alpha);
+            soundNames = "**"+ event.getGuild().getName() +"**:\n";
+            for(String k : sortedMap.keySet()) {
+                for(String s : sortedMap.get(k))
+                    soundNames+= s + " - ";
+            }
+        }
             event.reply(soundNames);
-        /*eb.setDescription("Lista con tutti i suoni del tier 1 bot");
-        eb.setColor(new Color(0, 128, 128));
-        eb.setAuthor(event.getSelfUser().getName(), "https://github.com/SafJNest",event.getSelfUser().getAvatarUrl());
-        eb.setFooter("*Questo non e' soundfx, questa e' perfezione cit. steve jobs (probabilmente)", null);
-        File file = new File("rsc" + File.separator + "img" + File.separator + "mp3.png");
-        eb.setThumbnail("attachment://mp3.png");
-        channel.sendMessageEmbeds(eb.build())
-                    .addFile(file, "mp3.png")
-                    .queue();
-        */
+        
 	}
 }
