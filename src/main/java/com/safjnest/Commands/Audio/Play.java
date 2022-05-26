f86a1759dafea8399959276a3709e331798398ad
LΞON <leonardopanichi4@gmail.com>
2022-05-26 20:46:09 +0200

    finally add search to play

--- src/main/java/com/safjnest/Commands/Audio/Play.java
+++ src/main/java/com/safjnest/Commands/Audio/Play.java
@@ -1,7 +1,14 @@
 package com.safjnest.Commands.Audio;
 
 import java.awt.Color;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.safjnest.Utilities.SafJNest;
 import com.safjnest.Utilities.TrackScheduler;
@@ -10,6 +17,13 @@ import com.jagrosh.jdautilities.command.CommandEvent;
 import com.safjnest.Utilities.AudioHandler;
 import com.safjnest.Utilities.JSONReader;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
@@ -30,28 +44,71 @@ import net.dv8tion.jda.api.entities.MessageChannel;
  * @since 1.0
  */
 public class Play extends Command {
+    private String youtubeApiKey;
     private HashMap<String,String> tierOneLink;
 
-    public Play(HashMap<String,String> tierOneLink){
+    public Play(String youtubeApiKey, HashMap<String,String> tierOneLink){
         this.name = this.getClass().getSimpleName();
         this.aliases = new JSONReader().getArray(this.name, "alias");
         this.help = new JSONReader().getString(this.name, "help");
         this.cooldown = new JSONReader().getCooldown(this.name);
         this.category = new Category(new JSONReader().getString(this.name, "category"));
         this.arguments = new JSONReader().getString(this.name, "arguments");
+        this.youtubeApiKey = youtubeApiKey;
         this.tierOneLink = tierOneLink;
+    }
+
+    public static String getVideoIdFromYoutubeUrl(String youtubeUrl) {
+        /*
+            Possibile Youtube urls.
+            http://www.youtube.com/watch?v=dQw4w9WgXcQ
+            http://www.youtube.com/embed/dQw4w9WgXcQ
+            http://www.youtube.com/v/dQw4w9WgXcQ
+            http://www.youtube-nocookie.com/v/dQw4w9WgXcQ?version=3&hl=en_US&rel=0
+            http://www.youtube.com/watch?v=dQw4w9WgXcQ
+            http://www.youtube.com/watch?feature=player_embedded&v=dQw4w9WgXcQ
+            http://www.youtube.com/e/dQw4w9WgXcQ
+            http://youtu.be/dQw4w9WgXcQ
+        */
+        String pattern = "(?<=watch\\?v=|/videos/|embed\\/|youtu.be\\/|\\/v\\/|\\/e\\/|watch\\?v%3D|watch\\?feature=player_embedded&v=|%2Fvideos%2F|embed%\u200C\u200B2F|youtu.be%2F|%2Fv%2F)[^#\\&\\?\\n]*";
+        Pattern compiledPattern = Pattern.compile(pattern);
+        //url is youtube url for which you want to extract the id.
+        Matcher matcher = compiledPattern.matcher(youtubeUrl);
+        if (matcher.find()) {
+            return matcher.group();
+        }
+        return null;
     }
 
 	@Override
 	protected void execute(CommandEvent event) {
-        String toPlay = event.getArgs();
         if(event.getMember().getVoiceState().getChannel() == null){
             event.reply("Non sei in un canale vocale.");
             return;
         }
 
+        String toPlay = getVideoIdFromYoutubeUrl(event.getArgs());
+        if(toPlay == null){
+            String keyword = event.getArgs();
+            keyword = keyword.replace(" ", "+");
+            String url = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + keyword + "&key=" + youtubeApiKey;
+            System.out.println(url);
+            try {
+                URL theUrl = new URL(url);
+                URLConnection request = theUrl.openConnection();
+                request.connect();
+                JSONParser parser = new JSONParser();
+                JSONObject json = (JSONObject) parser.parse(new InputStreamReader((InputStream) request.getContent()));
+                JSONArray items = (JSONArray) json.get("items");
+                JSONObject item = (JSONObject) items.get(0);
+                JSONObject id = (JSONObject) item.get("id");
+                toPlay = (String) id.get("videoId");
+            } catch (IOException | ParseException e) {
+                e.printStackTrace();
+            }
+        }
+
         MessageChannel channel = event.getChannel();
-        EmbedBuilder eb = new EmbedBuilder();
         AudioChannel myChannel = event.getMember().getVoiceState().getChannel();
         AudioManager audioManager = event.getGuild().getAudioManager();
         AudioPlayerManager playerManager = new DefaultAudioPlayerManager();
@@ -90,9 +147,12 @@ public class Play extends Command {
                 event.reply(throwable.getMessage());
             }
         });
+
         player.playTrack(trackScheduler.getTrack());
         if(player.getPlayingTrack() == null)
             return;
+
+        EmbedBuilder eb = new EmbedBuilder();
         eb = new EmbedBuilder();
         eb.setTitle("In riproduzione:");
         eb.addField("Durata", SafJNest.getFormattedDuration(player.getPlayingTrack().getInfo().length) , true);
