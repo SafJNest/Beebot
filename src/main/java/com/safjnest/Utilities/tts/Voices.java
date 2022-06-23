ee0505fb6d8159587c070b795ce823bf892f62e2
Lorenzo Sanseverino <lorenzosanseverino2003@gmail.com>
2022-06-23 14:51:10 +0200

    improving tts

--- src/main/java/com/safjnest/Utilities/tts/Voices.java
+++ src/main/java/com/safjnest/Utilities/tts/Voices.java
@@ -2,6 +2,8 @@ package com.safjnest.Utilities.tts;
 
 public class Voices {
   public class English_UnitedStates {
+    public static final String id = "en-us";
+
     public static final String Linda = "Linda";
     
     public static final String Amy = "Amy";
@@ -14,6 +16,8 @@ public class Voices {
   }
   
   public class Russian {
+    public static final String id = "zh-cn";
+
     public static final String Olga = "Olga";
     
     public static final String Marina = "Marina";
@@ -22,10 +26,14 @@ public class Voices {
   }
   
   public class Danish {
+    public static final String id = "da-dk";
+
     public static final String Freja = "Freja";
   }
   
   public class German_Germany {
+    public static final String id = "zh-cn";
+
     public static final String Hanna = "Hanna";
     
     public static final String Lina = "Lina";
@@ -34,14 +42,20 @@ public class Voices {
   }
   
   public class German_Austria {
+    public static final String id = "zh-cn";
+
     public static final String Lukas = "Lukas";
   }
   
   public class German_Switzerland {
+    public static final String id = "zh-cn";
+
     public static final String Tim = "Tim";
   }
   
   public class English_Australia {
+    public static final String id = "en-au";
+
     public static final String Zoe = "Zoe";
     
     public static final String Jack = "Jack";
@@ -52,6 +66,7 @@ public class Voices {
   }
   
   public class English_Canada {
+    public static final String id = "en-ca";
     public static final String Rose = "Rose";
     
     public static final String Clara = "Clara";
@@ -62,6 +77,8 @@ public class Voices {
   }
   
   public class English_GreatBritain {
+    public static final String id = "en-gb";
+
     public static final String Alice = "Alice";
     
     public static final String Nancy = "Nancy";
@@ -72,6 +89,8 @@ public class Voices {
   }
   
   public class English_India {
+    public static final String id = "en-in";
+
     public static final String Eka = "Eka";
     
     public static final String Jai = "Jai";
@@ -80,6 +99,8 @@ public class Voices {
   }
   
   public class Spanish_Spain {
+    public static final String id = "zh-cn";
+
     public static final String Camila = "Camila";
     
     public static final String Sofía = "Sofía";
@@ -90,6 +111,8 @@ public class Voices {
   }
   
   public class Spanish_Mexico {
+    public static final String id = "zh-cn";
+
     public static final String Juana = "Juana";
     
     public static final String Silvia = "Silvia";
@@ -100,10 +123,14 @@ public class Voices {
   }
   
   public class Finnish {
+    public static final String id = "fi-fi";
+
     public static final String Aada = "Aada";
   }
   
   public class French_Canada {
+    public static final String id = "fr-ca";
+
     public static final String Emile = "Emile";
     
     public static final String Olivia = "Olivia";
@@ -114,6 +141,8 @@ public class Voices {
   }
   
   public class French_France {
+    public static final String id = "fr-fr";
+
     public static final String Bette = "Bette";
     
     public static final String Iva = "Iva";
@@ -124,10 +153,14 @@ public class Voices {
   }
   
   public class French_Switzerland {
+    public static final String id = "fr-ch";
+
     public static final String Theo = "Theo";
   }
   
   public class Italian {
+    public static final String id = "it-it";
+
     public static final String Bria = "Bria";
     
     public static final String Pietro = "Pietro";
@@ -136,6 +169,8 @@ public class Voices {
   }
   
   public class Japanese {
+    public static final String id = "ja-jp";
+
     public static final String Hina = "Hina";
     
     public static final String Airi = "Airi";
@@ -146,32 +181,44 @@ public class Voices {
   }
   
   public class Korean {
+    public static final String id = "ko-kr";
+
     public static final String Nari = "Nari";
   }
   
   public class Norwegian {
+    public static final String id = "nb-no";
+
     public static final String Marte = "Marte";
     
     public static final String Erik = "Erik";
   }
   
   public class Dutch_Netherlands {
+    public static final String id = "nl-nl";
+
     public static final String Lotte = "Lotte";
     
     public static final String Bram = "Bram";
   }
   
   public class Dutch_Belgium {
+    public static final String id = "nl-be";
+
     public static final String Daan = "Daan";
   }
   
   public class Polish {
+    public static final String id = "pl-pl";
+
     public static final String Julia = "Julia";
     
     public static final String Jan = "Jan";
   }
   
   public class Portuguese_Brazil {
+    public static final String id = "pt-bn";
+
     public static final String Marcia = "Marcia";
     
     public static final String Ligia = "Ligia";
@@ -182,20 +229,28 @@ public class Voices {
   }
   
   public class Portuguese_Portugal {
+    public static final String id = "pt-pt";
+
     public static final String Leonor = "Leonor";
   }
   
   public class Catalan {
+    public static final String id = "ca-es";
+
     public static final String Rut = "Rut";
   }
   
   public class Swedish {
+    public static final String id = "sv-se";
+
     public static final String Molly = "Molly";
     
     public static final String Hugo = "Hugo";
   }
   
   public class Chinese_China {
+    public static final String id = "zh-cn";
+
     public static final String Luli = "Luli";
     
     public static final String Wang = "Wang";
@@ -206,6 +261,8 @@ public class Voices {
   }
   
   public class Chinese_HongKong {
+    public static final String id = "zh-hk";
+
     public static final String Jia = "Jia";
     
     public static final String Chen = "Chen";
@@ -214,6 +271,8 @@ public class Voices {
   }
   
   public class Chinese_Taiwan {
+    public static final String id = "zh-tw";
+
     public static final String Akemi = "Akemi";
     
     public static final String Lee = "Lee";
@@ -222,80 +281,118 @@ public class Voices {
   }
   
   public class English_Ireland {
+    public static final String id = "en-ie";
+
     public static final String Oran = "Oran";
   }
   
   public class Tamil {
+    public static final String id = "ta-in";
+
     public static final String Sai = "Sai";
   }
   
   public class Turkish {
+    public static final String id = "tr_TR";
+
     public static final String Omer = "Omer";
   }
   
   public class Arabic_SaudiArabia {
+    public static final String id = "ar-sa";
+
     public static final String Salim = "Salim";
   }
   
   public class Arabic_Egypt {
+    public static final String id = "ar-eg";
+
     public static final String Oda = "Oda";
   }
   
   public class Bulgarian {
+    public static final String id = "bg-bg";
+
     public static final String Dimo = "Dimo";
   }
   
   public class Croatian {
+    public static final String id = "hr-hr";
+
     public static final String Nikola = "Nikola";
   }
   
   public class Czech {
+    public static final String id = "cs-cz";
+
     public static final String Josef = "Josef";
   }
   
   public class Greek {
+    public static final String id = "el-gr";
+
     public static final String Neo = "Neo";
   }
   
   public class Hebrew {
+    public static final String id = "he-il";
+
     public static final String Rami = "Rami";
   }
   
   public class Hindi {
+    public static final String id = "hi-in";
+
     public static final String Puja = "Puja";
     
     public static final String Kabir = "Kabir";
   }
   
   public class Hungarian {
+    public static final String id = "hu-hu";
+
     public static final String Mate = "Mate";
   }
   
   public class Indonesian {
+    public static final String id = "id-id";
+
     public static final String Intan = "Intan";
   }
   
   public class Malay {
+    public static final String id = "ms-my";
+    
     public static final String Aqil = "Aqil";
   }
   
   public class Romanian {
+    public static final String id = "ro-ro";
+
     public static final String Doru = "Doru";
   }
   
   public class Slovak {
+    public static final String id = "sk-sk";
+
     public static final String Beda = "Beda";
   }
   
   public class Slovenian {
+    public static final String id = "sl-si";
+
     public static final String Vid = "Vid";
   }
   
   public class Thai {
+    public static final String id = "th-th";
+
     public static final String Ukrit = "Ukrit";
   }
   
   public class Vietnamese {
+    public static final String id = "vi-vn";
+
     public static final String Chi = "Chi";
   }
 }
