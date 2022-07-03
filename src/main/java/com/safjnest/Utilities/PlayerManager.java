976bf13187578355a2e6a0f8b9afbbd83fca703d
Lorenzo Sanseverino <lorenzosanseverino2003@gmail.com>
2022-07-03 12:31:47 +0200

    Update PlayerManager.java

--- src/main/java/com/safjnest/Utilities/PlayerManager.java
+++ src/main/java/com/safjnest/Utilities/PlayerManager.java
@@ -1,5 +1,17 @@
 package com.safjnest.Utilities;
 
 public class PlayerManager {
-    
+    private static String annodam = "Sul serio pensi che troveremo la voglia di fare qualcosa?";
+
+    public PlayerManager(){
+
+    }
+
+    public String whoIsASubHuman(){
+      return "THEPANTALUN"
+    }
+
+    public String toString(){
+      return annodam;
+    }
 }
