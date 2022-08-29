f61fb9eb64792280ecfdc8adcdb89ff7034414ae
Lorenzo Sanseverino <lorenzosanseverino2003@gmail.com>
2022-08-29 19:28:26 +0200

    something is start working

--- src/main/java/com/safjnest/Commands/LOL/Summoner.java
+++ src/main/java/com/safjnest/Commands/LOL/Summoner.java
@@ -15,13 +15,18 @@ import net.rithms.riot.api.RiotApiException;
 import net.dv8tion.jda.api.EmbedBuilder;
 import no.stelar7.api.r4j.basic.APICredentials;
 import no.stelar7.api.r4j.basic.constants.api.regions.LeagueShard;
+import no.stelar7.api.r4j.basic.constants.api.regions.RegionShard;
 import no.stelar7.api.r4j.impl.R4J;
 import no.stelar7.api.r4j.impl.R4J.LOLAPI;
 import no.stelar7.api.r4j.impl.lol.builders.champion.ChampionBuilder;
 import no.stelar7.api.r4j.impl.lol.builders.league.LeagueBuilder;
 import no.stelar7.api.r4j.impl.lol.raw.ChampionAPI;
 import no.stelar7.api.r4j.impl.lol.raw.LeagueAPI;
+import no.stelar7.api.r4j.impl.shared.AccountAPI;
 import no.stelar7.api.r4j.pojo.lol.champion.ChampionRotationInfo;
+import no.stelar7.api.r4j.pojo.lol.league.LeagueEntry;
+import no.stelar7.api.r4j.pojo.lol.league.LeagueEntryList;
+import no.stelar7.api.r4j.pojo.lol.league.LeagueItem;
 import no.stelar7.api.r4j.pojo.lol.match.v5.ChampionStats;
 import no.stelar7.api.r4j.pojo.lol.shared.BaseSpellData;
 import no.stelar7.api.r4j.pojo.lol.staticdata.champion.StaticChampion;
@@ -53,8 +58,27 @@ public class Summoner extends Command {
      */
 	@Override
 	protected void execute(CommandEvent event) {
-        no.stelar7.api.r4j.pojo.lol.summoner.Summoner s = new no.stelar7.api.r4j.pojo.lol.summoner.Summoner();
-        
+        String args = event.getArgs();
+        R4J r = new R4J(new APICredentials("RGAPI-9b796e85-3845-411f-ba86-a94f03f2a54f"));
+        no.stelar7.api.r4j.pojo.lol.summoner.Summoner s = r.getLoLAPI().getSummonerAPI().getSummonerByName(LeagueShard.EUW1, args);
+        System.out.println(s.getName());
+        try {
+            LeagueEntry entry = r.getLoLAPI().getLeagueAPI().getLeagueEntries(LeagueShard.EUW1, s.getSummonerId()).get(0);
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.setAuthor(event.getAuthor().getName());
+            builder.setColor(new Color(0,255,0));
+            builder.setThumbnail("https://ddragon.leagueoflegends.com/cdn/12.16.1/img/profileicon/"+s.getProfileIconId()+".png");
+            builder.addField("Level:", String.valueOf(s.getSummonerLevel()), true);
+            builder.addField("Rank", 
+                            entry.getTier() + " " + entry.getRank()+ " " +String.valueOf(entry.getLeaguePoints()) + " juicy lps\n"
+                            + entry.getWins() + "W/"+entry.getLosses()+"L\n"
+                            + "Winrate:" + Math.ceil((Double.valueOf(entry.getWins())/Double.valueOf(entry.getWins()+entry.getLosses()))*100)+"%", true);
+            event.reply(builder.build());
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 	}
 
 }
