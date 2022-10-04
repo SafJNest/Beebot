1cce96188c05a18fbdc1890d3a9357841c42c9c6
NeutronSun <lorenzosanseverino2003@gmail.com>
2022-10-04 14:13:28 +0200

    fix list with id

--- src/main/java/com/safjnest/Commands/Audio/List.java
+++ src/main/java/com/safjnest/Commands/Audio/List.java
@@ -5,10 +5,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
+import com.google.api.services.youtube.model.Member;
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import com.safjnest.Utilities.AwsS3;
 import com.safjnest.Utilities.JSONReader;
+import com.safjnest.Utilities.PostgreSQL;
 
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.User;
@@ -20,8 +22,9 @@ import net.dv8tion.jda.api.entities.User;
  */
 public class List extends Command {
     private AwsS3 s3Client;
+    private PostgreSQL sql;
 
-    public List(AwsS3 s3Client){
+    public List(AwsS3 s3Client, PostgreSQL sql){
         this.name = this.getClass().getSimpleName();;
         this.aliases = new JSONReader().getArray(this.name, "alias");
         this.help = new JSONReader().getString(this.name, "help");
@@ -29,92 +32,51 @@ public class List extends Command {
         this.category = new Category(new JSONReader().getString(this.name, "category"));
         this.arguments = new JSONReader().getString(this.name, "arguments");
         this.s3Client = s3Client;
+        this.sql = sql;
     }
 
 	@Override
 	protected void execute(CommandEvent event) {
-        HashMap<String, ArrayList<String>> alpha = null;
-        String[] args = event.getArgs().split(" ");
-        boolean isListMe = args[0].equals("me");
+        String query = "";
         String soundNames = "";
         int cont = 0;
-        if(isListMe){
-            alpha = s3Client.listObjectsByServer(event.getAuthor().getId(), event);
-            Map<String, ArrayList<String>> sortedMap = new TreeMap<>(alpha);
-            sortedMap.putAll(alpha);
-            for(String serverName : sortedMap.keySet()) {
-                soundNames += "**"+ serverName +"**" + ":\n";
-                for(String soundName : sortedMap.get(serverName)){
-                    soundNames += soundName + " - ";
-                    cont++;
-                }
-                soundNames = soundNames.substring(0, soundNames.length()-3) + "\n";
-            }
-        }else if(args[0].equalsIgnoreCase("server") || args[0].equalsIgnoreCase("")){
-            Guild guild = null;
-            if(args.length > 0){
+        if(event.getArgs().equals("server"))
+            query = "SELECT id, name, guild_id, user_id, extension FROM sound WHERE guild_id = '"+event.getGuild().getId()+"';";
+        else if(event.getArgs().equals("me"))
+            query = "SELECT id, name, guild_id, user_id, extension FROM sound WHERE user_id = '"+event.getAuthor().getId()+"';";
+        else if(event.getArgs().equals("global"))
+            query = "SELECT id, name, guild_id, user_id, extension FROM sound;";
+        else if(event.getArgs().split(" ")[0].equals("user")){
+            User theGuy = null;
+            if(event.getMessage().getMentions().getUsers().size() > 0){
+                theGuy = event.getMessage().getMentions().getUsers().get(0);
+            }else{
                 try {
-                    guild = event.getJDA().getGuildById(args[1]);
+                    theGuy = event.getJDA().getUserById(event.getArgs().split(" ")[1]);
                 } catch (Exception e) {
-                    guild = event.getGuild();
+                    event.reply("ID not found");
                 }
-                if(guild == null)
-                    guild = event.getGuild();
             }
-            else
-                guild = event.getGuild();
-
-            ArrayList<String> sounds = s3Client.listObjects(guild.getId());
-            soundNames = "**" + guild.getName() + "**:\n";
-            for(String s : sounds){
-                soundNames+= s + " - ";
+            //TODO: qualcosina ogni tanto da errore ora devo andare non faccio in tempo HEHE SAFj
+            query = "SELECT id, name, guild_id, user_id, extension FROM sound WHERE user_id = '"+theGuy.getId()+"';";
+        }
+        ArrayList<ArrayList<String>> arr = sql.getTuple(query, 5);
+        HashMap<String, ArrayList<String>> alpha = new HashMap<>();
+        for(int i = 0; i < arr.size(); i++){
+            if(!alpha.containsKey(arr.get(i).get(2)))
+                alpha.put(arr.get(i).get(2), new ArrayList<>());
+            alpha.get(arr.get(i).get(2)).add(arr.get(i).get(1));
+        }
+        Map<String, ArrayList<String>> sortedMap = new TreeMap<>(alpha);
+        sortedMap.putAll(alpha);
+        for(String serverId : sortedMap.keySet()) {
+            String serverName = (event.getJDA().getGuildById(serverId)!=null) ? event.getJDA().getGuildById(serverId).getName() : "Im not in the server"; 
+            soundNames += "**"+ serverName +"**" + ":\n";
+            for(String soundName : sortedMap.get(serverId)){
+                soundNames += soundName + " - ";
                 cont++;
             }
-            if(sounds.size() > 0)
-                soundNames = soundNames.substring(0, soundNames.length() - 3);
-        }else if(args[0].equalsIgnoreCase("user")){
-            User theGuy = null;
-            if(event.getMessage().getMentions().getMembers().size() > 0){
-                theGuy = event.getJDA().getUserById(event.getMessage().getMentions().getMembers().get(0).getId());
-                if(theGuy == null)
-                    theGuy = event.getAuthor();
-            }else if(args.length > 0){
-                try {
-                    theGuy = event.getJDA().getUserById(args[1]);
-                } catch (Exception e) {
-                    theGuy = event.getAuthor();
-                }
-                if(theGuy == null)
-                    theGuy = event.getAuthor();
-            }
-            else
-                theGuy = event.getAuthor();
-            alpha = s3Client.listObjectsByServer(theGuy.getId(), event);
-            Map<String, ArrayList<String>> sortedMap = new TreeMap<>(alpha);
-            sortedMap.putAll(alpha);
-            for(String serverName : sortedMap.keySet()) {
-                soundNames += "**"+ serverName +"**" + ":\n";
-                for(String soundName : sortedMap.get(serverName)){
-                    soundNames += soundName + " - ";
-                    cont++;
-                }
-                soundNames = soundNames.substring(0, soundNames.length()-3) + "\n";
-            }
-        }else if(args[0].equalsIgnoreCase("global")){
-            alpha = s3Client.listObjectsByServer();
-            Map<String, ArrayList<String>> sortedMap = new TreeMap<>(alpha);
-            sortedMap.putAll(alpha);
-            for(String serverName : sortedMap.keySet()) {
-                soundNames += "**" + 
-                ((event.getJDA().getGuildById(serverName) == null)
-                    ? "Server name not found."
-                    : event.getJDA().getGuildById(serverName).getName()) +"**" + ":\n";
-                for(String soundName : sortedMap.get(serverName)){
-                    soundNames += soundName.split("/")[2] + " - ";
-                    cont++;
-                }
-                soundNames = soundNames.substring(0, soundNames.length()-3) + "\n";
-            }
+            soundNames = soundNames.substring(0, soundNames.length()-3) + "\n";
         }
         soundNames += "\nTotal sounds: " + cont;
         event.reply(soundNames);
