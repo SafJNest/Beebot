c2f7db7d875129bb951470ca19adb4be93b17061
Lorenzo Sanseverino <lorenzosanseverino2003@gmail.com>
2022-11-14 11:22:02 +0100

    javadoc and fixing settings.json (activity)

--- src/main/java/com/safjnest/Utilities/TTSHandler.java
+++ src/main/java/com/safjnest/Utilities/TTSHandler.java
@@ -10,14 +10,31 @@ import com.safjnest.Utilities.tts.VoiceParameters;
 import com.safjnest.Utilities.tts.VoiceProvider;
 import com.safjnest.Utilities.tts.Voices;
 
+/**
+ * Class that provides to generate {@code .mp3} files using TTS
+ * @author <a href="https://github.com/NeutronSun">NeutronSun</a>
+ */
 public class TTSHandler {
+    /**
+     * The TTS provider
+     * @see <a href="https://www.voicerss.org/sdk/java.aspx"> Voice RSS</a>
+     */
     private VoiceProvider tts;
     String path = "rsc" + File.separator + "tts";
 
+    /**
+     * Default constructor
+     * @param token
+     */
     public TTSHandler(String token){
         tts = new VoiceProvider(token);
     }
 
+    /**
+     * Generates a {@code .mp3} gived a speech and the italian voice name
+     * @param speech {@code String} with the text to be read
+     * @param userName {@code String} name's voice
+     */
     public void makeSpeech(String speech, String userName) {
         for (File file : new File(path).listFiles())
             file.delete();
@@ -40,6 +57,13 @@ public class TTSHandler {
         }
     }
 
+    /**
+     * Generates a {@code .mp3} gived a speech, the language and voice name
+     * @param speech {@code String} with the text to be read
+     * @param userName {@code String} user who ask the tts
+     * @param voiceName {@code String} voice name
+     * @param language {@code String} languages of the voice
+     */
     public void makeSpeech(String speech, String userName, String voiceName, String language) {
         for (File file : new File(path).listFiles())
             file.delete();
