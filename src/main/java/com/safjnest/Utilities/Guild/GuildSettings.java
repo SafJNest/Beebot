c3b47a36f22b6a94c51499103fad1818d03deecd
Lorenzo Sanseverino <lorenzosanseverino2003@gmail.com>
2023-05-03 20:20:12 +0200

    cant focking coding
    ERROR net.dv8tion.jda.api.requests.RestAction - RestAction queue returned failure: [ErrorResponseException] 30034: Max number of daily application command creates has been reached (200)

--- src/main/java/com/safjnest/Utilities/Guild/GuildSettings.java
+++ src/main/java/com/safjnest/Utilities/Guild/GuildSettings.java
@@ -73,7 +73,7 @@ public class GuildSettings {
     public GuildData retrieveServer(String stringId) {
         String query = "SELECT * FROM guild_settings WHERE guild_id = '" + stringId + "' AND bot_id = '" + botId + "';";
         ArrayList<String> guildArrayList = DatabaseHandler.getSql().getSpecifiedRow(query, 0);
-        GuildData guild = (guildArrayList == null) 
+        GuildData guild = (guildArrayList == null || guildArrayList.get(2) == null) 
                     ? new GuildData(Long.parseLong(stringId), prefix) 
                     : new GuildData(Long.parseLong(guildArrayList.get(0)), guildArrayList.get(2));
         saveData(guild);
