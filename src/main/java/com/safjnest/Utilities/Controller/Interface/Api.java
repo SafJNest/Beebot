c4bc7c6a9309747b44caf46f45023b42894828a8
Sunyx <lorenzosanseverino2003@gmail.com>
2024-01-19 22:42:16 +0100

    test 1

---
+++ src/main/java/com/safjnest/Utilities/Controller/Interface/Api.java
@@ -0,0 +1,7 @@
+package com.safjnest.Utilities.Controller.Interface;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Api extends JpaRepository<ApiClass, Integer> {
+    
+}
