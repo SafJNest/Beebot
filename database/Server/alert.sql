da96be7e250dcc62bf4037e97741238917657251
Sunyx <lorenzosanseverino2003@gmail.com>
2024-03-02 00:19:09 +0100

    update backup

--- database/Server/alert.sql
+++ database/Server/alert.sql
@@ -1,21 +1,15 @@
 CREATE TABLE
   `alert` (
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
     `guild_id` varchar(19) NOT NULL,
     `bot_id` varchar(19) NOT NULL,
-    `boost_channel` varchar(19) DEFAULT NULL,
-    `leave_channel` varchar(19) DEFAULT NULL,
-    `welcome_channel` varchar(19) DEFAULT NULL,
-    `boost_message` text DEFAULT NULL,
-    `leave_message` text DEFAULT NULL,
-    `levelup_message` text DEFAULT NULL,
-    `welcome_message` text DEFAULT NULL,
-    `boost_enabled` tinyint(1) DEFAULT 0,
-    `leave_enabled` tinyint(1) DEFAULT 0,
-    `levelup_enabled` tinyint(1) DEFAULT 0,
-    `welcome_enabled` tinyint(1) DEFAULT 0,
-    `welcome_role` varchar(19) DEFAULT NULL,
-    PRIMARY KEY (`guild_id`, `bot_id`),
-    KEY `alert_bot_relation` (`bot_id`),
-    CONSTRAINT `alert_bot_relation` FOREIGN KEY (`bot_id`) REFERENCES `bots` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT `alert_guild_relation` FOREIGN KEY (`guild_id`) REFERENCES `guilds` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-  ) ENGINE = InnoDB DEFAULT CHARSET = latin1
+    `message` text NOT NULL DEFAULT '',
+    `channel` varchar(19) DEFAULT NULL,
+    `enabled` tinyint(4) NOT NULL,
+    `type` tinyint(4) NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `alert2_relation_1` (`guild_id`),
+    KEY `alert2_relation_2` (`bot_id`),
+    CONSTRAINT `alert2_relation_1` FOREIGN KEY (`guild_id`) REFERENCES `guilds` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `alert2_relation_2` FOREIGN KEY (`bot_id`) REFERENCES `bots` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  ) ENGINE = InnoDB AUTO_INCREMENT = 22 DEFAULT CHARSET = latin1
