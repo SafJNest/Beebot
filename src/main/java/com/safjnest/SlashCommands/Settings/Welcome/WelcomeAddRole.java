efd46154e0146937ebb7bc0e9f915cac04d8fcc2
Sunyx <lorenzosanseverino2003@gmail.com>
2024-03-04 21:32:49 +0100

    add welcome add/remove role, fix bugs on insert role

---
+++ src/main/java/com/safjnest/SlashCommands/Settings/Welcome/WelcomeAddRole.java
@@ -0,0 +1,56 @@
+package com.safjnest.SlashCommands.Settings.Welcome;
+
+import java.util.Arrays;
+
+import com.jagrosh.jdautilities.command.SlashCommand;
+import com.jagrosh.jdautilities.command.SlashCommandEvent;
+import com.safjnest.Utilities.CommandsLoader;
+import com.safjnest.Utilities.Bot.BotDataHandler;
+import com.safjnest.Utilities.Bot.Guild.GuildData;
+import com.safjnest.Utilities.Bot.Guild.Alert.AlertData;
+import com.safjnest.Utilities.Bot.Guild.Alert.AlertType;
+
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+
+public class WelcomeAddRole extends SlashCommand{
+
+    public WelcomeAddRole(String father){
+        this.name = this.getClass().getSimpleName().replace("Slash", "").replace(father, "").toLowerCase();
+        this.help = new CommandsLoader().getString(this.name, "help", father.toLowerCase());
+        this.cooldown = new CommandsLoader().getCooldown(this.name, father.toLowerCase());
+        this.category = new Category(new CommandsLoader().getString(father.toLowerCase(), "category"));
+        this.options = Arrays.asList(
+            new OptionData(OptionType.ROLE, "role", "Role that will be given to the new members.", true)
+        );
+    }
+
+    @Override
+    protected void execute(SlashCommandEvent event) {
+        String roleID = event.getOption("role") != null ? event.getOption("role").getAsString() : null;
+
+        String guildId = event.getGuild().getId();
+        String botId = event.getJDA().getSelfUser().getId();
+
+        GuildData gs = BotDataHandler.getSettings(botId).getGuildSettings().getServer(guildId);
+        
+        AlertData welcome = gs.getAlert(AlertType.WELCOME);
+
+        if (welcome == null) {
+            event.deferReply(true).addContent("This guild doesn't have a welcome message.").queue();
+            return;
+        }
+
+        if (welcome.getRoles() != null && welcome.getRoles().containsValue(roleID)) {
+            event.deferReply(true).addContent("This role is already in the welcome message.").queue();
+            return;
+        }
+
+        if(!welcome.addRole(roleID)) {
+            event.deferReply(true).addContent("Something went wrong.").queue();
+            return;
+        }
+
+        event.deferReply(false).addContent("Add welcome role.").queue();
+    }
+}
