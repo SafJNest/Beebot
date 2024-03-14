8f4bcf349409f20e94e1049f57d9735456922139
Sun <lorenzosanseverino2003@gmail.com>
2024-03-14 15:20:58 +0100

    send a message on channeldelete
    now if you delete a channel which is used as alert/blacklist the bot will send a message

--- src/main/java/com/safjnest/App.java
+++ src/main/java/com/safjnest/App.java
@@ -35,7 +35,7 @@ public class App {
     /**
      * Insane beebot core
      */
-    private static boolean extremeTesting = false;
+    private static boolean extremeTesting = true;
 
     public static boolean isExtremeTesting() {
         return extremeTesting;
