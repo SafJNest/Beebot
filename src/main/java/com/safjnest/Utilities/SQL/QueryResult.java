6cb3d9a1c83d5f280f5768213f9ac811d1bd4cca
Sunyx <lorenzosanseverino2003@gmail.com>
2024-04-15 23:54:59 +0200

    refactor: custom command better declaration and queries

--- src/main/java/com/safjnest/Utilities/SQL/QueryResult.java
+++ src/main/java/com/safjnest/Utilities/SQL/QueryResult.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class QueryResult implements Iterable<ResultRow>{
 
@@ -49,5 +50,10 @@ public class QueryResult implements Iterable<ResultRow>{
         }
         return list;
     }
+
+    public Stream<ResultRow> stream() {
+        return result.stream();
+    }
+
 
 }
