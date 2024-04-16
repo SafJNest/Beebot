2c0723783b43dff4953d61319ff7200eb7e76640
Sunyx <lorenzosanseverino2003@gmail.com>
2024-04-16 22:43:46 +0200

    refactor: extreme eventhandler

--- src/main/java/com/safjnest/Utilities/EventHandlers/EventHandlerBeebot.java
+++ src/main/java/com/safjnest/Utilities/EventHandlers/EventHandlerBeebot.java
@@ -6,7 +6,6 @@ import com.safjnest.Bot;
 import com.safjnest.Utilities.Functions;
 import com.safjnest.Utilities.UserData;
 import com.safjnest.Utilities.Guild.GuildData;
-import com.safjnest.Utilities.Guild.GuildSettings;
 
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -23,21 +22,12 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
  */
 public class EventHandlerBeebot extends ListenerAdapter {
 
-        private GuildSettings settings;
-
-    /**
-     * Constructor for the TheListenerBeebot class.
-     */
-    public EventHandlerBeebot(GuildSettings settings) {
-        this.settings = settings;
-    }
-
     @Override
     public void onMessageReceived(MessageReceivedEvent e) {
         if (e.getAuthor().isBot())
             return;
 
-        GuildData guildData = settings.getServer(e.getGuild().getId());
+        GuildData guildData = Bot.getGuildData(e.getGuild().getId());
         UserData userData = Bot.getUserData(e.getAuthor().getId());
         
         Functions.handleAlias(guildData, userData, e);
--- src/main/java/com/safjnest/Utilities/Functions.java
+++ src/main/java/com/safjnest/Utilities/Functions.java
@@ -1,22 +1,39 @@
 package com.safjnest.Utilities;
 
+import java.io.File;
+import java.util.HashMap;
+
 import com.jagrosh.jdautilities.command.CommandClient;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import com.safjnest.Bot;
-
+import com.safjnest.Utilities.Audio.PlayerManager;
+import com.safjnest.Utilities.Guild.BlacklistData;
 import com.safjnest.Utilities.Guild.GuildData;
 import com.safjnest.Utilities.Guild.MemberData;
 import com.safjnest.Utilities.Guild.Alert.AlertData;
+import com.safjnest.Utilities.Guild.Alert.AlertKey;
 import com.safjnest.Utilities.Guild.Alert.AlertType;
 import com.safjnest.Utilities.Guild.Alert.RewardData;
+import com.safjnest.Utilities.SQL.DatabaseHandler;
+import com.safjnest.Utilities.SQL.QueryResult;
+import com.safjnest.Utilities.SQL.ResultRow;
+import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.UserSnowflake;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle;
 
 public class Functions {
     
@@ -29,7 +46,7 @@ public class Functions {
         
         String command = message.getContentRaw().substring(prefix.length());
 
-        AliasData alias = userData.getAlias(command);
+        AliasData alias = userData.getAliases().get(command);
         if (alias == null)
             return;
         
@@ -121,6 +138,200 @@ public class Functions {
             e.getChannel().asTextChannel().sendMessage(message).queue();
             return;
         }
+    }
+
+    public static void handleAlert(User theGuy, Guild guild, AlertType type) {
+        MessageChannel channel = null;
+
+        AlertData alert = Bot.getGuildSettings().getServer(guild.getId()).getAlert(type);
+        if (alert == null || !alert.isValid()) {
+            return;
+        }
+
+        String channel_id = alert.getChannelId();
+        if (channel_id == null || channel_id.equals("") || channel_id.equals("null")) {
+            return;
+        }
+
+        channel = guild.getTextChannelById(channel_id);
+        if (channel == null) {
+            return;
+        }
+
+        String message = alert.getMessage().replace("#user", theGuy.getAsMention());
+        if (alert.isPrivate()) {
+            theGuy.openPrivateChannel().queue(channelPrivate -> {
+                channelPrivate.sendMessage(message).queue();
+            });
+        } else {
+            channel.sendMessage(message).queue();
+        }
+
+        if (alert.getRoles() != null) {
+            String[] roles = alert.getRoles().values().toArray(new String[0]);
+            givesRoles(theGuy, guild, roles);
+        }
+    }
+
+
+    public static void givesRoles(User theGuy, Guild guild, String[] roles) {
+        if (roles == null || roles.length == 0) {
+            return;
+        }
+        for (String role : roles) {
+            Role r = guild.getRoleById(role);
+            if (r == null) {
+                continue;
+            }
+
+            guild.addRoleToMember(theGuy, r).queue();
+        }
+    }
+
+    public static void handleBlacklist(User badGuy, Guild guild) {
+        MessageChannel channel = null;
+        GuildData guildData = Bot.getGuildSettings().getServer(guild.getId());
+
+
+        int threshold = guildData.getThreshold();
+        if(threshold == 0)
+            return;
+        
+        int times = DatabaseHandler.getBlacklistBan(badGuy.getId());
+
+        if(!guildData.blacklistEnabled() || guildData.getThreshold() == 0 || guildData.getThreshold() > times)
+            return;
+        
+        EmbedBuilder eb = new EmbedBuilder();
+        eb.setAuthor(guild.getJDA().getSelfUser().getName());
+        eb.setThumbnail(badGuy.getAvatarUrl());
+        eb.setTitle(":radioactive:Blacklist:radioactive:");
+        eb.setDescription("The new member " + badGuy.getAsMention() + " is on the blacklist for being banned in " + times + " different guilds.\nYou have the discretion to choose the next steps.");
+
+        channel = guild.getTextChannelById(guildData.getBlackChannelId());
+
+        Button kick = Button.primary("kick-" + badGuy.getId(), "Kick");
+        Button ban = Button.primary("ban-" + badGuy.getId(), "Ban");
+        Button ignore = Button.primary("ignore-" + badGuy.getId(), "Ignore");
+
+
+        kick = kick.withStyle(ButtonStyle.PRIMARY);
+        ban = ban.withStyle(ButtonStyle.PRIMARY);
+        ignore = ignore.withStyle(ButtonStyle.SUCCESS);
+        channel.sendMessageEmbeds(eb.build()).addActionRow(ignore, kick, ban).queue();
+    }
+
+    public static void handleBlacklistAlert(User badGuy, Guild guild) {
+        GuildData guildData = Bot.getGuildSettings().getServer(guild.getId());
+        int threshold = guildData.getThreshold();
+
+        if(threshold == 0)
+            return;
+        
+        DatabaseHandler.insertUserBlacklist(badGuy.getId(), guild.getId());
+
+        int times = 0;
+        times = times + DatabaseHandler.getBannedTimes(badGuy.getId());
+
+        QueryResult guilds = DatabaseHandler.getGuildByThreshold(times, guild.getId());
+        if(guilds == null)
+            return;
+        
+        EmbedBuilder eb = new EmbedBuilder();
+        eb.setAuthor(guild.getJDA().getSelfUser().getName());
+        eb.setThumbnail(badGuy.getAvatarUrl());
+        eb.setTitle(":radioactive:Blacklist:radioactive:");
+        eb.setDescription("The member " + badGuy.getAsMention() + " is on the blacklist for being banned in " + times + " different guilds.\nYou have the discretion to choose the next steps.");
+        for(ResultRow g : guilds){
+            Guild gg = guild.getJDA().getGuildById(g.get("guild_id"));
+            if(gg.getMemberById(badGuy.getId()) == null)
+                continue;
+
+            TextChannel channel = gg.getTextChannelById(g.get("blacklist_channel"));
+
+            Button kick = Button.primary("kick-" + badGuy.getId(), "Kick");
+            Button ban = Button.primary("ban-" + badGuy.getId(), "Ban");
+            Button ignore = Button.primary("ignore-" + badGuy.getId(), "Ignore");
+
+            kick = kick.withStyle(ButtonStyle.PRIMARY);
+            ban = ban.withStyle(ButtonStyle.PRIMARY);
+            ignore = ignore.withStyle(ButtonStyle.SUCCESS);
+            channel.sendMessageEmbeds(eb.build()).addActionRow(ignore, kick, ban).queue();
+        }
+    }
+
+    public static void handleChannelDeleteAlert(Guild guild, String channelID) {
+        GuildData g = Bot.getGuildSettings().getServer(guild.getId());
+
+        String alertChannel = guild.getDefaultChannel().getId();
+        String alertMessage = "";
+        String content = "";
+        HashMap<AlertKey, AlertData> alerts = g.getAlerts();
+        BlacklistData bld = g.getBlacklistData();
+        if (alerts != null) {
+            for (AlertData data : alerts.values()) {
+                if (data.getChannelId() != null && data.getChannelId().equals(channelID)) {
+                    data.setAlertChannel(null);
+                    content += data.getType().getDescription() + ", ";
+                }
+            }
+        }
+        if (bld != null) {
+            if (bld.getBlackChannelId() != null && bld.getBlackChannelId().equals(channelID)) {
+                bld.setBlackChannelId(null);
+                content += "Blacklist";
+            }
+        }
+        if (!content.equals("")) {
+            alertMessage = "These alerts need to be modified as the channel has been canceled:\n" + content;
+            guild.getTextChannelById(alertChannel).sendMessage(alertMessage).queue();
+        }
+    }
+
+
+    public static boolean isBotAlone(AudioChannel botChannel, AudioChannel channelLeave) {
+        if((botChannel != null && channelLeave != null) && (botChannel.getId().equals(channelLeave.getId()))
+        && (channelLeave.getMembers().stream().filter(member -> !member.getUser().isBot()).count() == 0)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void handleBotLeave(Guild guild) {
+        User self = guild.getJDA().getSelfUser();
+        
+        guild.getAudioManager().closeAudioConnection();
+        PlayerManager.get().getGuildMusicManager(guild, self).getTrackScheduler().clearQueue();
+    }
+
+
+    public static void handleGreetSound(AudioChannel channelJoin, User theGuy, Guild guild) {
+        ResultRow sound = DatabaseHandler.getGreet(theGuy.getId(), guild.getId());
+        if(sound.emptyValues())
+            return;
+
+        PlayerManager pm = PlayerManager.get();
+
+        String path = "rsc" + File.separator + "SoundBoard"+ File.separator + sound.get("id") + "." + sound.get("extension");
+
+        pm.loadItemOrdered(guild, theGuy, path, new AudioLoadResultHandler() {
+            @Override
+            public void trackLoaded(AudioTrack track) {
+                pm.getGuildMusicManager(guild, theGuy).getTrackScheduler().playForce(track);
+                guild.getAudioManager().openAudioConnection(channelJoin);
+            }
+
+            @Override
+            public void playlistLoaded(AudioPlaylist playlist) {}
+            
+            @Override
+            public void noMatches() {}
+
+            @Override
+            public void loadFailed(FriendlyException throwable) {
+                System.out.println("error: " + throwable.getMessage());
+            }
+        });
     }
 
 
