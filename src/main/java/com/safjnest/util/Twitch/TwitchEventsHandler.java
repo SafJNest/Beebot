819f8a9459fea629fbe5429dc549878492b16b04
Sunyx <lorenzosanseverino2003@gmail.com>
2024-07-05 00:04:57 +0200

    fix: stream object null

--- src/main/java/com/safjnest/util/Twitch/TwitchEventsHandler.java
+++ src/main/java/com/safjnest/util/Twitch/TwitchEventsHandler.java
@@ -9,6 +9,7 @@ import com.safjnest.core.Bot;
 import com.safjnest.sql.DatabaseHandler;
 import com.safjnest.sql.QueryResult;
 import com.safjnest.sql.ResultRow;
+import com.safjnest.util.PermissionHandler;
 import com.safjnest.util.log.BotLogger;
 
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -29,16 +30,29 @@ class TwitchEventsHandler {
         
 
         Stream stream = TwitchClient.getStream(event.getBroadcasterUserId());
+        if (stream == null) {
+            try {Thread.sleep(10000); } 
+            catch (InterruptedException e) { }
+            
+            stream = TwitchClient.getStream(event.getBroadcasterUserId());
+            if (stream == null) {
+                BotLogger.error("[TWITCH] Stream is null for " + event.getBroadcasterUserName());
+                PermissionHandler.getUntouchables().forEach((id) -> Bot.getJDA().retrieveUserById(id).complete().openPrivateChannel().queue((privateChannel) -> privateChannel.sendMessage("dio ladro schifoso animale non va twitch leggi la console svegliati").queue()));
+            }
+        }
 
         String liveUrl = "https://www.twitch.tv/" + streamer.getLogin();
 
         EmbedBuilder eb = new EmbedBuilder();
-        eb.setTitle(!stream.getTitle().isEmpty() ? stream.getTitle() : "No Title", liveUrl);
-        eb.setThumbnail("https://static-cdn.jtvnw.net/ttv-boxart/" + stream.getGameId() + "-285x380.jpg");
+        if (stream != null) {
+            eb.setTitle(!stream.getTitle().isEmpty() ? stream.getTitle() : "No Title", liveUrl);
+            eb.setThumbnail("https://static-cdn.jtvnw.net/ttv-boxart/" + stream.getGameId() + "-285x380.jpg");
+            eb.addField("Game", stream.getGameName(), true);
+            eb.setImage(stream.getThumbnailUrl(400, 225));
+        }
+        
         eb.setAuthor(streamer.getDisplayName() + " is now live on Twitch!", liveUrl, streamer.getProfileImageUrl());
-        eb.addField("Game", stream.getGameName(), true);
         eb.setColor(Bot.getColor());
-        eb.setImage(stream.getThumbnailUrl(400, 225));
         eb.setFooter("Twitch", "https://static-00.iconduck.com/assets.00/twitch-icon-512x512-ws2eyit3.png");
         eb.setTimestamp(Instant.now());
 
