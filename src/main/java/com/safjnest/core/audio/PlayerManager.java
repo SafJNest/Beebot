3c893df919ae7e86d1c37ea5e884180aefe0d195
LΞON <79836803+Leon412@users.noreply.github.com>
2024-08-20 19:10:18 +0200

    fix: remove lavalink and fix pom

--- src/main/java/com/safjnest/core/audio/PlayerManager.java
+++ src/main/java/com/safjnest/core/audio/PlayerManager.java
@@ -1,9 +1,5 @@
 package com.safjnest.core.audio;
 
-import java.net.Inet6Address;
-import java.net.InetAddress;
-import java.net.URI;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -11,7 +7,6 @@ import java.util.Map;
 import java.util.concurrent.Future;
 
 import com.safjnest.App;
-import com.safjnest.core.Bot;
 import com.safjnest.util.SettingsLoader;
 import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
@@ -22,9 +17,6 @@ import com.sedmelluq.lava.extensions.youtuberotator.planner.AbstractRoutePlanner
 import com.sedmelluq.lava.extensions.youtuberotator.planner.RotatingIpRoutePlanner;
 import com.sedmelluq.lava.extensions.youtuberotator.tools.ip.*;
 
-
-import dev.arbjerg.lavalink.client.LavalinkClient;
-import dev.arbjerg.lavalink.client.NodeOptions;
 import dev.lavalink.youtube.clients.Web;
 import net.dv8tion.jda.api.entities.Guild;
 
@@ -41,36 +33,32 @@ public class PlayerManager {
     private static PlayerManager INSTANCE;
     private Map<String, GuildMusicManager> guildMusicManagers = new HashMap<>();
     private AudioPlayerManager audioPlayerManager = new DefaultAudioPlayerManager();
-    private LavalinkClient lavalink;
+    //private LavalinkClient lavalink; //i hope we dont have to use this bullshit
 
     public PlayerManager() {
-        SettingsLoader settingsLoader = new SettingsLoader(App.getBot(), null);
+        //lavalink = new LavalinkClient(Long.parseLong(Bot.getBotId()));
+        //lavalink.addNode(new NodeOptions.Builder("beebot", URI.create(settingsLoader.getLavalinkHost()), settingsLoader.getLavalinkPassword(), null, 0).build());
 
-        lavalink = new LavalinkClient(Long.parseLong(Bot.getBotId()));
-        lavalink.addNode(new NodeOptions.Builder("beebot", URI.create(settingsLoader.getLavalinkHost()), settingsLoader.getLavalinkPassword(), null, 0).build());
-
-        loadYoutube(settingsLoader);
+        registerYoutube();
         
         AudioSourceManagers.registerRemoteSources(audioPlayerManager);
         AudioSourceManagers.registerLocalSource(audioPlayerManager);
     }
 
     @SuppressWarnings("rawtypes")
-    private void loadYoutube(SettingsLoader settingsLoader) {
+    private void registerYoutube() {
+        SettingsLoader settingsLoader = new SettingsLoader(App.getBot(), null);
+
         dev.lavalink.youtube.YoutubeAudioSourceManager youtubeAudioSourceManager = new dev.lavalink.youtube.YoutubeAudioSourceManager();
+
         Web.setPoTokenAndVisitorData(settingsLoader.getPoToken(), settingsLoader.getVisitorData());
 
         audioPlayerManager.registerSourceManager(youtubeAudioSourceManager);
 
-        
         IpBlock ipBlock = new Ipv6Block("2001:470:1f04:3a::/64");
-        
         List<IpBlock> ipBlocks = Collections.singletonList(ipBlock);
-        
         AbstractRoutePlanner routePlanner = new RotatingIpRoutePlanner(ipBlocks);
-
         YoutubeIpRotatorSetup rotator = new YoutubeIpRotatorSetup(routePlanner);
-
         rotator.forConfiguration(youtubeAudioSourceManager.getHttpInterfaceManager(), false)
             .withMainDelegateFilter(youtubeAudioSourceManager.getContextFilter())
             .setup();
