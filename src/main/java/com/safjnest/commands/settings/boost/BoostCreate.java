0b261923bee078ff8033aa136254db3f066bedcd
Lorenzo Sanseverino <72988879+NeutronSun@users.noreply.github.com>
2025-08-21 10:38:46 +0200

    alert-v2: remove useless commands

--- src/main/java/com/safjnest/commands/settings/boost/BoostCreate.java
+++ src/main/java/com/safjnest/commands/settings/boost/BoostCreate.java
@@ -1,6 +1,5 @@
 package com.safjnest.commands.settings.boost;
 
-import java.util.Arrays;
 
 import com.jagrosh.jdautilities.command.SlashCommand;
 import com.jagrosh.jdautilities.command.SlashCommandEvent;
@@ -9,12 +8,9 @@ import com.safjnest.model.guild.GuildData;
 import com.safjnest.model.guild.alert.AlertData;
 import com.safjnest.model.guild.alert.AlertSendType;
 import com.safjnest.model.guild.alert.AlertType;
+import com.safjnest.util.AlertMessage;
 import com.safjnest.util.BotCommand;
 import com.safjnest.util.CommandsLoader;
-
-import net.dv8tion.jda.api.entities.channel.ChannelType;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
-import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
 public class BoostCreate extends SlashCommand {
 
@@ -27,57 +23,18 @@ public class BoostCreate extends SlashCommand {
         this.cooldown = commandData.getCooldown();
         this.category = commandData.getCategory();
         
-        this.options = Arrays.asList(
-            new OptionData(OptionType.STRING, "message", "Boost message", true),
-            new OptionData(OptionType.CHANNEL, "channel", "Boost channel (leave out to use the guild's system channel).", false)
-                .setChannelTypes(ChannelType.TEXT),
-            new OptionData(OptionType.STRING, "sendtype", "How the message would be sent", false)
-                .addChoice("Channel", String.valueOf(AlertSendType.CHANNEL.ordinal()))
-                .addChoice("Private", String.valueOf(AlertSendType.PRIVATE.ordinal()))
-                .addChoice("Both", String.valueOf(AlertSendType.BOTH.ordinal())),
-            new OptionData(OptionType.STRING, "private_message", "If empty would be use the same message (Must enable the private option (private or both)", false)
-        );
-
         commandData.setThings(this);
     }
 
     @Override
     protected void execute(SlashCommandEvent event) {
-        String boostText = event.getOption("message").getAsString();
-        String privateText = event.getOption("private_message") != null ? event.getOption("private_message").getAsString() : null;
-        AlertSendType sendType = event.getOption("sendtype") != null ? AlertSendType.values()[event.getOption("sendtype").getAsInt()] : AlertSendType.CHANNEL;
-        
-        String channelID;
-        if(event.getOption("channel") != null) 
-            channelID = event.getOption("channel").getAsString();
-        else if(event.getGuild().getSystemChannel() != null) 
-            channelID = event.getGuild().getSystemChannel().getId();
-        else{
-            event.deferReply(true).addContent("No channel specified and no system channel found.").queue();
-            return;
-        }
-
         String guildId = event.getGuild().getId();
-
         GuildData gs = GuildCache.getGuildOrPut(guildId);
-
-        AlertData boost = gs.getAlert(AlertType.BOOST);
-
-        if(boost != null) {
-            event.deferReply(true).addContent("A boost message already exists.").queue();
-            return;
+        AlertData alert = gs.getAlert(AlertType.BOOST);
+        if(alert == null) {
+            alert =  new AlertData(guildId, "", "", null, AlertSendType.CHANNEL, AlertType.BOOST);
+            gs.getAlerts().put(alert.getKey(), alert);
         }
-
-        AlertData newBoost = new AlertData(guildId, boostText, privateText, channelID, sendType, AlertType.BOOST);
-        
-        if(newBoost.getID() == 0) {
-            event.deferReply(true).addContent("Something went wrong.").queue();
-            return;
-        }
-
-        gs.getAlerts().put(newBoost.getKey(), newBoost);
-
-
-        event.deferReply(false).addContent("Boost message created.").queue();
+        event.deferReply().addComponents(AlertMessage.build(gs, alert)).useComponentsV2().queue();
     }
 }
