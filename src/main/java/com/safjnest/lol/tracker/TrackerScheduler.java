abf2c525435963e6e695a4df39272324f7e4fa98
Lorenzo Sanseverino <lorenzosanseverino117@gmail.com>
2026-08-15 14:39:21 +0200

    Update TrackerScheduler.java

--- src/main/java/com/safjnest/lol/tracker/TrackerScheduler.java
+++ src/main/java/com/safjnest/lol/tracker/TrackerScheduler.java
@@ -26,7 +26,7 @@ public class TrackerScheduler {
         started = true;
         startedAt = System.currentTimeMillis();
 
-        //if (App.isTesting()) return;
+        if (App.isTesting()) return;
 
         ChronoTask track = () -> retrieveSummoners();
         track.scheduleAtFixedRate(0, TimeConstant.MINUTE * 10, TimeUnit.MILLISECONDS);
